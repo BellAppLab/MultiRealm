1820e0083eefb6d0160030affc3db6f542453870
Bell App Lab <apps@bellapplab.com>
2015-08-11 17:35:10 +0100

    Fixed initialization methods to properly create Realms in the designated NSOperationQueue.

--- Pod/Classes/MultiRealm.swift
+++ Pod/Classes/MultiRealm.swift
@@ -39,21 +39,61 @@ import RealmSwift
 public typealias MultiRealmBlock = ()->()
 
 
-public struct MultiRealm
+public final class MultiRealm
 {
-    public let realm: Realm
-    private var queue: NSOperationQueue?
+    public var realm: Realm!
+    private let queue: NSOperationQueue
     
-    public init(realm: Realm, inBackground background: Bool)
-    {
-        if background {
-            self.queue = NSOperationQueue()
-            self.queue!.maxConcurrentOperationCount = 1
-            self.queue!.name = "MultiRealmQueue"
-        } else {
+    // MARK: Initializers
+    
+    /**
+    Obtains a Realm instance persisted at the specified file path. Defaults to
+    `Realm.defaultPath`
+    
+    :param: path Path to the realm file.
+    :param: inBackground: Determines if the Realm should be created on a background thread
+    */
+    public convenience init(path: String = Realm.defaultPath, inBackground background: Bool) {
+        self.init(path: path, readOnly: false, encryptionKey: nil, inBackground: background)
+    }
+    
+    /**
+    Obtains a `Realm` instance with persistence to a specific file path with
+    options.
+    
+    Like `init(path:)`, but with the ability to open read-only realms and get
+    errors as an `NSError` inout parameter rather than exceptions.
+    
+    :warning: Read-only Realms do not support changes made to the file while the
+    `Realm` exists. This means that you cannot open a Realm as both read-only
+    and read-write at the same time. Read-only Realms should normally only be used
+    on files which cannot be opened in read-write mode, and not just for enforcing
+    correctness in code that should not need to write to the Realm.
+    
+    :param: path            Path to the file you want the data saved in.
+    :param: readOnly        Bool indicating if this Realm is read-only (must use for read-only files).
+    :param: encryptionKey   64-byte key to use to encrypt the data.
+    :param: inBackground: Determines if the Realm should be created on a background thread
+    that describes the problem. If you are not interested in
+    possible errors, omit the argument, or pass in `nil`.
+    */
+    public init(path: String, readOnly: Bool, encryptionKey: NSData? = nil, inBackground background: Bool) {
+        if !background {
             assert(NSThread.currentThread() == NSThread.mainThread(), "Calls to init with 'inBackground = false' must be made from the main thread")
+            self.queue = NSOperationQueue.mainQueue()
+        } else {
+            self.queue = NSOperationQueue()
+            self.queue.maxConcurrentOperationCount = 1
+            self.queue.name = "MultiRealmQueue"
         }
-        self.realm = realm
+        self.performBlock { [unowned self] () -> () in
+            var error = NSErrorPointer()
+            if let realm = Realm(path: path, readOnly: readOnly, encryptionKey: encryptionKey, error: error) {
+                self.realm = realm
+            } else {
+                NSException(name: NSInternalInconsistencyException, reason: error.debugDescription, userInfo: nil).raise()
+            }
+        }
     }
     
     public func performBlock(block: MultiRealmBlock)
@@ -64,11 +104,7 @@ public struct MultiRealm
             endBackgroundTask(bgTaskId)
         }
         
-        if var queue = self.queue {
-            queue.addOperationWithBlock(finalBlock)
-            return
-        }
-        dispatch_async(dispatch_get_main_queue(), finalBlock)
+        self.queue.addOperationWithBlock(finalBlock)
     }
 }
 
